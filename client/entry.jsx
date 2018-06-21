e8f6f4aca5ae4be1f62edfd3691206cebe5fb9c5
Ken Tran <loschorts@gmail.com>
2018-06-20 19:14:04 -0700

    valid req, cors blocker

--- client/entry.jsx
+++ client/entry.jsx
@@ -13,6 +13,28 @@ const history = createHistory()
 
 const store = configureStore(history);
 
+const buildUrl = (url, params) => {
+	const _url = new URL(url);
+	Object.keys(params).forEach(key => _url.searchParams.append(key, params[key]));
+	return _url;
+};
+
+console.log("HELLO");
+
+const url = "https://maps.googleapis.com/maps/api/directions/json?";
+const key = "AIzaSyDieL1nnb3nrdILx7KKJGDjhRLiJ40zDzU";
+
+const origin = "1518 4th Avenue, Oakland, CA";
+const destination = "Pacifica State Beach, Pacifica, CA"
+const traffic_model = "best_guess";
+const departure_time = "now";
+const mode = "driving";
+
+const params = { key, traffic_model, departure_time, mode, origin, destination };
+
+fetch(buildUrl(url, params), { mode: "cors" })
+	.then((res) => { console.dir(res); });
+
 const App = () => (
 	<AppProvider store={store}>
 		<AppRouter history={history}/>
